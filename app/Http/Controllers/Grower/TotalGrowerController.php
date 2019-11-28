afe9a49a0b3b012400c706507dea7efb88f950f7
fardeenkhan786 <fardenand2013@gmail.com>
2019-11-28 17:42:12 +0530

    Bootstrap Integration Done

---
+++ app/Http/Controllers/Grower/TotalGrowerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Grower;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TotalGrowerController extends Controller
+{
+    //
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Teaboard Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    @include('navbar')
+    <br>
+
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-md-2">
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
